676528016088afc25a72731818ca265f0d1b6d5b
Petrik Diak <diak@petrik.hu>
2024-11-19 16:08:14 +0100

    GET and DELETE is done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Delete, Get, HttpCode, NotFoundException, Param, Post, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Konyv } from './konyv';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
@@ -12,4 +13,63 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
+
+  konyvek: Konyv[] = [
+    {
+      id: 0,
+      title: "Hobbit",
+      author: "Tolkien",
+      isbn: "idk",
+      publishYear: 42,
+      reserved: true
+    },
+
+    {
+      id: 1,
+      title: "LoTR: Fellowship of the Ring",
+      author: "Tolkien",
+      isbn: "idk2",
+      publishYear: 1954,
+      reserved: false
+    },
+    {
+      id: 2,
+      title: "Dune",
+      author: "Frank Herbert",
+      isbn: "idk3",
+      publishYear: 1965,
+      reserved: true
+    }
+  ]
+  nextID = 3;
+  @Get('konyvek')
+  konyvListazas() {
+    return this.konyvek;
+  }
+
+
+  @Get('konyvek/:konyvid')
+  konvyvIDalapjan(@Param('konyvid') id: string) {
+    const idSzam = parseInt(id);
+    const konyv = this.konyvek.find(konyv => konyv.id == idSzam);
+    if (!konyv) {
+      throw new NotFoundException("ID wrong")
+    }
+    return konyv;
+  }
+
+  @Delete('konyvek/:konyvid')
+  @HttpCode(204)
+  konyvTorles(@Param('konyvid') id: string){
+    
+    const idSzam = parseInt(id);
+    const konyvIndex = this.konyvek.findIndex(konyv => konyv.id == idSzam);
+    if (konyvIndex == 0) {
+      throw new NotFoundException("ID wrong")
+    }
+    this.konyvek.splice(konyvIndex,1);
+  }
+
+
+
 }
